ef67d6d32d79939e4ac4a6112fda447a4023546c
dnb8866 <dnb8866@gmail.com>
2024-05-01 12:24:04 +0700

    init

--- run.py
+++ run.py
@@ -1,18 +1,25 @@
+import os
 import paramiko
 import time
 
 from datetime import date
+from dotenv import load_dotenv
 
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 client = paramiko.SSHClient()
 
 
 def ssh_connect(func):
     def wrapper(*args, **kwargs):
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname=,
-                       username=,
-                       password=,
-                       port=)
+        client.connect(hostname=os.getenv("HOSTNAME"),
+                       username=os.getenv("USERNAME"),
+                       password=os.getenv("PASSWORD"),
+                       port=os.getenv("PORT_SSH")
+                       )
         data = func(*args, **kwargs)
         client.close()
         return data
@@ -27,4 +34,5 @@ def ssh_command(command) -> str:
 
 while True:
     ssh_command(f'pg_dump -U denis family_finances > /home/denis/db/dump_db_family_finances_{date.today()}.sql')
+    print("ok")
     time.sleep(28800)
